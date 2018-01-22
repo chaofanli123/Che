599b60ceec1ff2880fabe17c25ea6b652bf54d32
jcc2018 <719949442@qq.com>
2018-01-22 14:33:52 +0800

    通知下达

--- app/src/main/java/com/victor/che/bean/Notify.java
+++ app/src/main/java/com/victor/che/bean/Notify.java
@@ -9,24 +9,121 @@ import java.util.List;
 
 public class Notify implements Serializable {
 
+
     /**
-     * type : 1
-     * title : 测试
-     * content : 文本A
-     * files : ,
-     * status : 1
-     * readNum : 0
-     * unReadNum : 2
-     * isSelf : false
-     * oaNotifyRecordList : []
-     * createBy : {"loginFlag":"1","roleList":[],"delFlag":"0","id":"1","isNewRecord":false}
-     * createDate : 2018-01-12 18:00:19
-     * updateBy : {"loginFlag":"1","roleList":[],"delFlag":"0","id":"1","isNewRecord":false}
-     * updateDate : 2018-01-12 18:00:19
-     * delFlag : 0
-     * id : 5b628443d4e34279bb960b7d391a0dc1
-     * isNewRecord : false
+     * page : {"pageNo":1,"pageSize":10,"count":1,"list":[{"id":"5b628443d4e34279bb960b7d391a0dc1","isNewRecord":false,"remarks":null,"createDate":"2018-01-12 10:00:19","updateDate":"2018-01-12 10:00:19","type":"1","title":"测试","content":"文本A","files":",","status":"1","readNum":"0","unReadNum":"2","readFlag":null,"oaNotifyRecordList":[],"oaNotifyRecordIds":"","oaNotifyRecordNames":"","self":false}],"maxResults":10,"firstResult":0,"html":"<div class=\"fixed-table-pagination\" style=\"display: block;\"><div class=\"pull-left pagination-detail\"><span class=\"pagination-info\">显示第 1 到第 1 条记录，总共 1 条记录<\/span><span class=\"page-list\">每页显示 <span class=\"btn-group dropup\"><button type=\"button\" class=\"btn btn-default  btn-outline dropdown-toggle\" data-toggle=\"dropdown\" aria-expanded=\"false\"><span class=\"page-size\">10<\/span> <span class=\"caret\"><\/span><\/button><ul class=\"dropdown-menu\" role=\"menu\"><li class=\"active\"><a href=\"javascript:page(1,10,'');\">10<\/a><\/li><li class=\"\"><a href=\"javascript:page(1,25,'');\">25<\/a><\/li><li class=\"\"><a href=\"javascript:page(1,50,'');\">50<\/a><\/li><li class=\"\"><a href=\"javascript:page(1,100,'');\">100<\/a><\/li><\/ul><\/span> 条记录<\/span><\/div><div class=\"pull-right pagination-roll\"><ul class=\"pagination pagination-outline\"><li class=\"paginate_button previous disabled\"><a href=\"javascript:\"><i class=\"fa fa-angle-double-left\"><\/i><\/a><\/li>\n<li class=\"paginate_button previous disabled\"><a href=\"javascript:\"><i class=\"fa fa-angle-left\"><\/i><\/a><\/li>\n<li class=\"paginate_button active\"><a href=\"javascript:\">1<\/a><\/li>\n<li class=\"paginate_button next disabled\"><a href=\"javascript:\"><i class=\"fa fa-angle-right\"><\/i><\/a><\/li>\n<li class=\"paginate_button next disabled\"><a href=\"javascript:\"><i class=\"fa fa-angle-double-right\"><\/i><\/a><\/li>\n<\/ul><\/div><\/div>"}
      */
+
+    private PageBean page;
+
+    public PageBean getPage() {
+        return page;
+    }
+
+    public void setPage(PageBean page) {
+        this.page = page;
+    }
+
+    public static class PageBean {
+        /**
+         * pageNo : 1
+         * pageSize : 10
+         * count : 1
+         * list : [{"id":"5b628443d4e34279bb960b7d391a0dc1","isNewRecord":false,"remarks":null,"createDate":"2018-01-12 10:00:19","updateDate":"2018-01-12 10:00:19","type":"1","title":"测试","content":"文本A","files":",","status":"1","readNum":"0","unReadNum":"2","readFlag":null,"oaNotifyRecordList":[],"oaNotifyRecordIds":"","oaNotifyRecordNames":"","self":false}]
+         * maxResults : 10
+         * firstResult : 0
+         * html : <div class="fixed-table-pagination" style="display: block;"><div class="pull-left pagination-detail"><span class="pagination-info">显示第 1 到第 1 条记录，总共 1 条记录</span><span class="page-list">每页显示 <span class="btn-group dropup"><button type="button" class="btn btn-default  btn-outline dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="page-size">10</span> <span class="caret"></span></button><ul class="dropdown-menu" role="menu"><li class="active"><a href="javascript:page(1,10,'');">10</a></li><li class=""><a href="javascript:page(1,25,'');">25</a></li><li class=""><a href="javascript:page(1,50,'');">50</a></li><li class=""><a href="javascript:page(1,100,'');">100</a></li></ul></span> 条记录</span></div><div class="pull-right pagination-roll"><ul class="pagination pagination-outline"><li class="paginate_button previous disabled"><a href="javascript:"><i class="fa fa-angle-double-left"></i></a></li>
+         <li class="paginate_button previous disabled"><a href="javascript:"><i class="fa fa-angle-left"></i></a></li>
+         <li class="paginate_button active"><a href="javascript:">1</a></li>
+         <li class="paginate_button next disabled"><a href="javascript:"><i class="fa fa-angle-right"></i></a></li>
+         <li class="paginate_button next disabled"><a href="javascript:"><i class="fa fa-angle-double-right"></i></a></li>
+         </ul></div></div>
+         */
+
+        private int pageNo;
+        private int pageSize;
+        private int count;
+        private int maxResults;
+        private int firstResult;
+        private String html;
+        private List<ListBean> list;
+
+        public int getPageNo() {
+            return pageNo;
+        }
+
+        public void setPageNo(int pageNo) {
+            this.pageNo = pageNo;
+        }
+
+        public int getPageSize() {
+            return pageSize;
+        }
+
+        public void setPageSize(int pageSize) {
+            this.pageSize = pageSize;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
+        }
+
+        public int getMaxResults() {
+            return maxResults;
+        }
+
+        public void setMaxResults(int maxResults) {
+            this.maxResults = maxResults;
+        }
+
+        public int getFirstResult() {
+            return firstResult;
+        }
+
+        public void setFirstResult(int firstResult) {
+            this.firstResult = firstResult;
+        }
+
+        public String getHtml() {
+            return html;
+        }
+
+        public void setHtml(String html) {
+            this.html = html;
+        }
+
+        public List<ListBean> getList() {
+            return list;
+        }
+
+        public void setList(List<ListBean> list) {
+            this.list = list;
+        }
+
+        public static class ListBean implements Serializable{
+            /**
+             * id : 5b628443d4e34279bb960b7d391a0dc1
+             * isNewRecord : false
+             * remarks : null
+             * createDate : 2018-01-12 10:00:19
+             * updateDate : 2018-01-12 10:00:19
+             * type : 1
+             * title : 测试
+             * content : 文本A
+             * files : ,
+             * status : 1
+             * readNum : 0
+             * unReadNum : 2
+             * readFlag : null
+             * oaNotifyRecordList : []
+             * oaNotifyRecordIds :
+             * oaNotifyRecordNames :
+             * self : false
+             */
 
             private String id;
             private boolean isNewRecord;
@@ -46,243 +143,141 @@ public class Notify implements Serializable {
             private boolean self;
             private List<?> oaNotifyRecordList;
 
-    public String getType() {
-        return type;
-    }
+            public String getId() {
+                return id;
+            }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+            public void setId(String id) {
+                this.id = id;
+            }
 
-    public String getTitle() {
-        return title;
-    }
+            public boolean isIsNewRecord() {
+                return isNewRecord;
+            }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+            public void setIsNewRecord(boolean isNewRecord) {
+                this.isNewRecord = isNewRecord;
+            }
 
-    public String getContent() {
-        return content;
-    }
+            public Object getRemarks() {
+                return remarks;
+            }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+            public void setRemarks(Object remarks) {
+                this.remarks = remarks;
+            }
 
-    public String getFiles() {
-        return files;
-    }
+            public String getCreateDate() {
+                return createDate;
+            }
 
-    public void setFiles(String files) {
-        this.files = files;
-    }
+            public void setCreateDate(String createDate) {
+                this.createDate = createDate;
+            }
 
-    public String getStatus() {
-        return status;
-    }
+            public String getUpdateDate() {
+                return updateDate;
+            }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+            public void setUpdateDate(String updateDate) {
+                this.updateDate = updateDate;
+            }
 
-    public String getReadNum() {
-        return readNum;
-    }
+            public String getType() {
+                return type;
+            }
 
-    public void setReadNum(String readNum) {
-        this.readNum = readNum;
-    }
+            public void setType(String type) {
+                this.type = type;
+            }
 
-    public String getUnReadNum() {
-        return unReadNum;
-    }
+            public String getTitle() {
+                return title;
+            }
 
-    public void setUnReadNum(String unReadNum) {
-        this.unReadNum = unReadNum;
-    }
+            public void setTitle(String title) {
+                this.title = title;
+            }
 
-    public boolean isIsSelf() {
-        return isSelf;
-    }
+            public String getContent() {
+                return content;
+            }
 
-    public void setIsSelf(boolean isSelf) {
-        this.isSelf = isSelf;
-    }
+            public void setContent(String content) {
+                this.content = content;
+            }
 
-    public CreateByBean getCreateBy() {
-        return createBy;
-    }
+            public String getFiles() {
+                return files;
+            }
 
-    public void setCreateBy(CreateByBean createBy) {
-        this.createBy = createBy;
-    }
+            public void setFiles(String files) {
+                this.files = files;
+            }
 
-    public String getCreateDate() {
-        return createDate;
-    }
+            public String getStatus() {
+                return status;
+            }
 
-    public void setCreateDate(String createDate) {
-        this.createDate = createDate;
-    }
+            public void setStatus(String status) {
+                this.status = status;
+            }
 
-    public UpdateByBean getUpdateBy() {
-        return updateBy;
-    }
+            public String getReadNum() {
+                return readNum;
+            }
 
-    public void setUpdateBy(UpdateByBean updateBy) {
-        this.updateBy = updateBy;
-    }
+            public void setReadNum(String readNum) {
+                this.readNum = readNum;
+            }
 
-    public String getUpdateDate() {
-        return updateDate;
-    }
+            public String getUnReadNum() {
+                return unReadNum;
+            }
 
-    public void setUpdateDate(String updateDate) {
-        this.updateDate = updateDate;
-    }
+            public void setUnReadNum(String unReadNum) {
+                this.unReadNum = unReadNum;
+            }
 
-    public String getDelFlag() {
-        return delFlag;
-    }
+            public Object getReadFlag() {
+                return readFlag;
+            }
 
-    public void setDelFlag(String delFlag) {
-        this.delFlag = delFlag;
-    }
+            public void setReadFlag(Object readFlag) {
+                this.readFlag = readFlag;
+            }
 
-    public String getId() {
-        return id;
-    }
+            public String getOaNotifyRecordIds() {
+                return oaNotifyRecordIds;
+            }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+            public void setOaNotifyRecordIds(String oaNotifyRecordIds) {
+                this.oaNotifyRecordIds = oaNotifyRecordIds;
+            }
 
-    public boolean isIsNewRecord() {
-        return isNewRecord;
-    }
+            public String getOaNotifyRecordNames() {
+                return oaNotifyRecordNames;
+            }
 
-    public void setIsNewRecord(boolean isNewRecord) {
-        this.isNewRecord = isNewRecord;
-    }
+            public void setOaNotifyRecordNames(String oaNotifyRecordNames) {
+                this.oaNotifyRecordNames = oaNotifyRecordNames;
+            }
 
-    public List<?> getOaNotifyRecordList() {
-        return oaNotifyRecordList;
-    }
+            public boolean isSelf() {
+                return self;
+            }
 
-    public void setOaNotifyRecordList(List<?> oaNotifyRecordList) {
-        this.oaNotifyRecordList = oaNotifyRecordList;
-    }
+            public void setSelf(boolean self) {
+                this.self = self;
+            }
 
-    public static class CreateByBean {
-        /**
-         * loginFlag : 1
-         * roleList : []
-         * delFlag : 0
-         * id : 1
-         * isNewRecord : false
-         */
+            public List<?> getOaNotifyRecordList() {
+                return oaNotifyRecordList;
+            }
 
-        private String loginFlag;
-        private String delFlag;
-        private String id;
-        private boolean isNewRecord;
-        private List<?> roleList;
-
-        public String getLoginFlag() {
-            return loginFlag;
-        }
-
-        public void setLoginFlag(String loginFlag) {
-            this.loginFlag = loginFlag;
-        }
-
-        public String getDelFlag() {
-            return delFlag;
-        }
-
-        public void setDelFlag(String delFlag) {
-            this.delFlag = delFlag;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public boolean isIsNewRecord() {
-            return isNewRecord;
-        }
-
-        public void setIsNewRecord(boolean isNewRecord) {
-            this.isNewRecord = isNewRecord;
-        }
-
-        public List<?> getRoleList() {
-            return roleList;
-        }
-
-        public void setRoleList(List<?> roleList) {
-            this.roleList = roleList;
-        }
-    }
-
-    public static class UpdateByBean {
-        /**
-         * loginFlag : 1
-         * roleList : []
-         * delFlag : 0
-         * id : 1
-         * isNewRecord : false
-         */
-
-        private String loginFlag;
-        private String delFlag;
-        private String id;
-        private boolean isNewRecord;
-        private List<?> roleList;
-
-        public String getLoginFlag() {
-            return loginFlag;
-        }
-
-        public void setLoginFlag(String loginFlag) {
-            this.loginFlag = loginFlag;
-        }
-
-        public String getDelFlag() {
-            return delFlag;
-        }
-
-        public void setDelFlag(String delFlag) {
-            this.delFlag = delFlag;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public boolean isIsNewRecord() {
-            return isNewRecord;
-        }
-
-        public void setIsNewRecord(boolean isNewRecord) {
-            this.isNewRecord = isNewRecord;
-        }
-
-        public List<?> getRoleList() {
-            return roleList;
-        }
-
-        public void setRoleList(List<?> roleList) {
-            this.roleList = roleList;
+            public void setOaNotifyRecordList(List<?> oaNotifyRecordList) {
+                this.oaNotifyRecordList = oaNotifyRecordList;
+            }
         }
     }
 }
